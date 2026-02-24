3a2998d7a82f68b0159185694adee9a6fcda5bba
sasikiran0601 <sasikiran0601@gmail.com>
2026-02-24 22:17:03 +0530

    Changed app.py

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ app.add_middleware(
 )
 
 # Static files & templates
-
-templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 # Request body model
 class ChatRequest(BaseModel):
